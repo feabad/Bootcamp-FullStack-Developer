7e87deb6b43488f00a8438ddba7f97d032faf3fa
Fernanda Abad <fe_sanchez@icloud.com>
2022-05-02 16:12:56 -0700

    Atualização de arquivos

---
+++ 02 Internet/Projetos/index.php
@@ -0,0 +1,19 @@
+<html>
+
+	<head>
+		<title>Meu primeiro site em PHP! Woohoo!</title>
+	<head>
+	
+	<body>
+	
+		<?php
+						
+			for( $i = 0 ; $i < 10 ; $i++ ) {
+				print( "Fernanda Sanchez Abad Linha número " . $i . "<br ;>");
+			}
+			
+		?>
+		
+	</body>
+	
+</html>
